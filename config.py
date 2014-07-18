3c03e0a8ee9e8da3bd5984a7f316d5d0d7f6a467
x0x01 <aka.x0x01@gmail.com>
2014-07-18 07:31:17 +0400

    Add: appfog database example

--- config.py
+++ config.py
@@ -14,6 +14,19 @@ READABILITY_API_KEY = ''
 # DATABASE_URI = 'postgresql://scott:secret@localhost/mydatabase'
 # DATABASE_URI = 'mysql://scott:secret@localhost/mydatabase'
 # DATABASE_URI = 'sqlite:////absolute/path/to/foo.db'
+
+# appfog config
+# import os
+# import json
+# services = json.loads(os.getenv("VCAP_SERVICES", "{}"))
+# mysql_env = services["mysql-5.1"][0]["credentials"]
+# DATABASE_URI = "mysql://%s:%s@%s:%d/%s" %\
+#                (mysql_env['username'],
+#                 mysql_env['password'],
+#                 mysql_env['host'],
+#                 mysql_env['port'],
+#                 mysql_env['name'])
+
 DATABASE_URI = 'sqlite:///data.db'
 
 # debug ORM requests
